e5d9e44a606517abdd167c125e646ca67949dca7
CristianCepeda <ccepe014@fiu.edu>
2018-05-29 16:18:41 -0400

    Create-React-App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Profile from './Profile';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>GeekBookStore</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
