7eba4f0b0c5c60f3a9656d2d48281683b611c49f
CristianCepeda <ccepe014@fiu.edu>
2018-06-02 10:02:48 -0400

    fixed dependencies, added ejs for templating webpack for bundling files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App text-center">
         <h1>GeekBookStore</h1>
+        <div>
+          <p>Our Books Should Go Here</p>
+        </div>
       </div>
     );
   }
