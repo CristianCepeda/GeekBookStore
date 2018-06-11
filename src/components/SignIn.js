70842dc062824d53a549c598d9782fba4ef58958
CristianCepeda <ccepe014@fiu.edu>
2018-06-11 13:10:53 -0400

    This is  my latest copy

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -13,48 +13,50 @@ class SignIn extends Component {
         <div className="row align-items-center">
           <div className="col"></div>
 
-          <div className="col card bg-light">
+          <div className="col">
 
-            <div className="card-header text-center">
-              <ul className="nav nav-tabs card-header-tabs">
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">Sign In</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Create Account</a>
-                </li>
-              </ul>
-            </div>
+            <div className="card text-center bg-light">
+              <div className="card-header">
+                <ul className="nav nav-tabs card-header-tabs">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#">Sign In</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Create Account</a>
+                  </li>
+                </ul>
+              </div>
 
-            <div className="card-body">
-              <form>
-                <div className="form-label-group">
-                  <label htmlFor="inputEmail">Email Address</label>
-                  <input type="email" className="form-control" id="inputEmail" placeholder="Email address" required autoFocus/>
-                </div>
-                <div className="form-label-group">
-                  <label htmlFor="inputPassword">Password</label>
-                  <input type="password" className="form-control" id="inputPassword" placeholder="Password" required />
-                </div>
-                <button type="button" className="btn btn-primary btn-lg btn-block p-1 mt-3">Sign In</button>
-                <div className="container text-center">
-                  <p className="my-2"> ------------- or ------------- </p>
-                  <p>   Log in with  </p>
-                  <div className="row">
-                    <Link to="./https://www.facebook.com/" className="col bg-primary text-white">
-                      <i className="fab fa-facebook"></i>
-                    </Link>
-                    <Link to="https://github.com/" className="col bg-primary text-white">
-                      <i className="fab fa-github"></i>
-                    </Link>
-                    <Link to="https://www.google.com/" className="col bg-primary text-white">
-                      <i className="fab fa-google"></i>
-                    </Link>
+              <div className="card-body">
+                <form>
+                  <div className="form-label-group">
+                    <label htmlFor="inputEmail">Email Address</label>
+                    <input type="email" className="form-control" id="inputEmail" placeholder="Email address" required autoFocus/>
                   </div>
-                </div>
-              </form>
-            </div>
+                  <div className="form-label-group">
+                    <label htmlFor="inputPassword">Password</label>
+                    <input type="password" className="form-control" id="inputPassword" placeholder="Password" required />
+                  </div>
+                  <button type="button" className="btn btn-primary btn-lg btn-block p-1 mt-3">Sign In</button>
+                  <div className="container text-center">
+                    <p className="my-2"> ------------- or ------------- </p>
+                    <p>   Log in with  </p>
+                    <div className="row">
+                      <Link to="./https://www.facebook.com/" className="col bg-primary text-white">
+                        <i className="fab fa-facebook"></i>
+                      </Link>
+                      <Link to="https://github.com/" className="col bg-primary text-white">
+                        <i className="fab fa-github"></i>
+                      </Link>
+                      <Link to="https://www.google.com/" className="col bg-primary text-white">
+                        <i className="fab fa-google"></i>
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
 
+            </div>
           </div>
 
           <div className="col"></div>
