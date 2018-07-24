e53df5f1e51dd47dc25e100dc0b0d3a3e3549eca
stephanie cliett <sclie001@fiu.edu>
2018-07-24 00:18:32 -0400

    Update HeaderNav.js
    my updated version of headernav that did not push earlier. designed a drop down instead of search box.

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -11,23 +11,38 @@ import SignIn from './SignIn';
 class HeaderNav extends Component {
   render() {
     return(
-      <header>
-        <div className="d-flex align-items-center py-0 px-md-4 mb-3 border-bottom box-shadow">
-          <div className="col d-flex text-center">
-            <Link to="/" className="Title p-2 flex-grow-1 navbar-brand text-secondary">GeekBookStore</Link>
-          </div>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+<div className="d-flex align-items-center py-0 px-md-4 mb-3 border-bottom box-shadow">
+  <div className="col d-flex text-center">
+    <Link to="/" className="Title p-2 flex-grow-1 navbar-brand text-secondary">GeekBookStore</Link>
+  </div>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  </div>
 
-          <div className="col-8 d-flex justify-content-center">
-            <form className="justify-content-center form-inline my-2 my-lg-0">
-              <input className="p-1 text-center form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            </form>
-          </div>
-
-          <div className="col-1 d-flex justify-content-end">
-            <Link to="/signin" className="btn btn-sm btn-outline-secondary">Sign In</Link>
-          </div>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Search a book
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Genre</a>
+          <a className="dropdown-item" href="#">top sellers</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">book rating</a>
         </div>
-      </header>
+      </li>
+    </ul>
+    <div className="col-1 d-flex justify-content-end">
+      <Link to="/signin" className="btn btn-sm btn-outline-secondary">Sign In</Link>
+    </div>
+  </div>
+</nav>
     );
   };
 };
